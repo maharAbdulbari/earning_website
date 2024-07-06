3ffb09e15ee8d4403fd165125ae821446d50c867
AbdulBari <maharabdulbari1@gmail.com>
2024-07-06 09:07:03 -0700

    website

---
+++ inde.js
@@ -0,0 +1,12 @@
+let button=document.getElementById('btn')
+
+// function call(){
+// buttons=alert("03249   send invest money");   
+// button.innerHTML= buttons;
+// }
+
+
+function call(){
+buttons=alert(" Thanks Dear ");   
+button.innerHTML= buttons;
+}
